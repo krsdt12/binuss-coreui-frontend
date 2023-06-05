294140ffdcf9a3c6cd7153bf5623aa5600b76f55
krsdt12 <krsdt1@gmail.com>
2023-06-06 06:03:18 +0700

    polishing

--- src/views/perusahaan/pt/pt.js
+++ src/views/perusahaan/pt/pt.js
@@ -1,44 +1,53 @@
-import { 
-  CCard, 
+import {
+  CCard,
   CButton,
-  CCardBody, 
-  CCardHeader, 
+  CCardBody,
+  CCardHeader,
   CTableHeaderCell,
   CCol,
-  CImage, 
-  CRow, 
-  CTable, 
-  CTableBody, 
-  CTableDataCell, 
-  CTableHead, 
-  CTableRow
-} from '@coreui/react'
-import React from 'react'
-import logo from '../../../assets/images/Logo/Binuss-logo.jpg'
-import { 
-  AiOutlineEye, 
-  AiOutlineEdit 
-} from "react-icons/ai"
-import CIcon from '@coreui/icons-react'
-import { cilPlus } from '@coreui/icons'
+  CImage,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableRow,
+  CContainer,
+} from "@coreui/react";
+import React from "react";
+import logo from "../../../assets/images/Logo/Binuss-logo.jpg";
+import { AiOutlineEye, AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+import CIcon from "@coreui/icons-react";
+import { cilPlus } from "@coreui/icons";
 
 const pt = () => {
   return (
-    <CRow>
+    <CContainer fluid>
       <CCol xs={12}>
         <CCard className="mb-4">
           <CCardHeader>
             <strong>Data PT</strong>
           </CCardHeader>
           <CCardBody>
-            <CCol xs={8}></CCol>
-            <CCol xs={4} className="align-middle">
-              <CButton color="primary" >
-                <CIcon icon={cilPlus} height={20}  className="align-middle" />
-                <span> Tambah Data</span>
-              </CButton>
-            </CCol>
-            <CTable>
+            <CRow>
+              <CCol align="right">
+                <CButton color="primary">
+                  <CRow>
+                    <CCol lg={3} md={3}>
+                      <CIcon
+                        icon={cilPlus}
+                        height={20}
+                        className="align-middle"
+                      />
+                    </CCol>
+                    <CCol className="d-none d-md-block">
+                      <span> Tambah Data</span>
+                    </CCol>
+                  </CRow>
+                </CButton>
+              </CCol>
+            </CRow>
+            <CTable align="middle" responsive>
               <CTableHead>
                 <CTableRow>
                   <CTableHeaderCell scope="col">#</CTableHeaderCell>
@@ -46,21 +55,36 @@ const pt = () => {
                   <CTableHeaderCell scope="col">Nama PT</CTableHeaderCell>
                   <CTableHeaderCell scope="col">Alamat</CTableHeaderCell>
                   <CTableHeaderCell scope="col">No. Telpon</CTableHeaderCell>
-                  <CTableHeaderCell scope="col" colSpan={2}>Action</CTableHeaderCell>
+                  <CTableHeaderCell scope="col" colSpan={3} class="text-center">
+                    Action
+                  </CTableHeaderCell>
                 </CTableRow>
               </CTableHead>
               <CTableBody>
-                <CTableRow className="align-middle">
+                <CTableRow>
                   <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell><CImage src={logo} alt="logo" height={50} /></CTableDataCell>
-                  <CTableDataCell>PT. BINA UTAMA SEJAHTERA SEJATI</CTableDataCell>
-                  <CTableDataCell>Boyolali</CTableDataCell>
-                  <CTableDataCell>08916627772</CTableDataCell>
                   <CTableDataCell>
-                    <CButton color="success" variant="ghost"><AiOutlineEdit /></CButton>
+                    <CImage src={logo} alt="logo" height={50} />
                   </CTableDataCell>
                   <CTableDataCell>
-                    <CButton color="info" variant="ghost"><AiOutlineEye /></CButton>
+                    PT. BINA UTAMA SEJAHTERA SEJATI
+                  </CTableDataCell>
+                  <CTableDataCell>Boyolali</CTableDataCell>
+                  <CTableDataCell>08916627772</CTableDataCell>
+                  <CTableDataCell width={50}>
+                    <CButton color="info" variant="ghost">
+                      <AiOutlineEye />
+                    </CButton>
+                  </CTableDataCell>
+                  <CTableDataCell width={50}>
+                    <CButton color="success" variant="ghost">
+                      <AiOutlineEdit />
+                    </CButton>
+                  </CTableDataCell>
+                  <CTableDataCell width={50}>
+                    <CButton color="danger" variant="ghost">
+                      <AiOutlineDelete />
+                    </CButton>
                   </CTableDataCell>
                 </CTableRow>
               </CTableBody>
@@ -68,8 +92,8 @@ const pt = () => {
           </CCardBody>
         </CCard>
       </CCol>
-    </CRow>
-  )
-}
+    </CContainer>
+  );
+};
 
-export default pt
+export default pt;
