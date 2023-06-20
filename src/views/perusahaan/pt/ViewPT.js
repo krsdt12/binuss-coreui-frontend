c6ae404e5497b61f1456d2a5d416008698c1c5ea
Qings41 <qings41@my365.id>
2023-06-20 15:58:51 +0700

    view pt

--- src/views/perusahaan/pt/ViewPT.js
+++ src/views/perusahaan/pt/ViewPT.js
@@ -3,37 +3,96 @@ import {
   CCardBody,
   CCardHeader,
   CCol,
+  CButtonGroup,
+  CButton,
   CContainer,
+  CNav,
+  CNavItem,
+  CNavLink,
   CImage,
   CRow,
 } from "@coreui/react";
 import React from "react";
 import logo from "../../../assets/images/Logo/Binuss-logo.jpg";
+import { AiOutlineLeft, AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+import CIcon from "@coreui/icons-react";
+import { cilLocationPin } from "@coreui/icons";
 
 const ViewPT = () => {
   return (
-    <CContainer lg>
-      <CCard>
-        <CCardBody>
-          <CRow className="pt-2">
-            <CCol lg={4} className="px-5 mb-5">
-              <CImage src={logo} thumbnail />
-            </CCol>
-            <CCol className="me-5">
-              <h2>PT BINUSS</h2>
-              <hr />
-              {/* <CRow>
-                <CCol xs={4}>Nama PT</CCol>
+    <CContainer>
+      <CRow>
+        <CCol lg={8} className="mb-3">
+          <CCard>
+            <CCardHeader>
+              <CRow>
+                <CCol sm={9} className="align-self-center">
+                  <span>
+                    <b>Profil PT</b>
+                  </span>
+                </CCol>
+                <CCol sm={3} align="right">
+                  <CButtonGroup role="group">
+                    <CButton color="secondary" variant="ghost">
+                      <AiOutlineLeft />
+                    </CButton>
+                    <CButton color="success" variant="ghost">
+                      <AiOutlineEdit />
+                    </CButton>
+                    <CButton color="danger" variant="ghost">
+                      <AiOutlineDelete />
+                    </CButton>
+                  </CButtonGroup>
+                </CCol>
+              </CRow>
+            </CCardHeader>
+            <CCardBody>
+              <CRow className="pt-2 ms-3 mb-3 me-3">
+                <CCol lg={4} className="mb-2">
+                  <CImage src={logo} thumbnail />
+                </CCol>
+                <CCol>
+                  <h2 className="mb-0">PT BINUSS</h2>
+                  <p style={{ color: "#9da5b1" }}>
+                    <CIcon icon={cilLocationPin} className="me-1"></CIcon>
+                    Boyolali
+                  </p>
+                  <hr />
+                </CCol>
+              </CRow>
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol lg={4} className="mb-3">
+          <CCard>
+            <CCardBody></CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+      <CRow className="mb-3">
+        <CCol>
+          <CCard>
+            <CNav variant="tabs">
+              <CNavItem>
+                <CNavLink href="#" active>
+                  Profil Perusahaan
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#">Data Karyawan</CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#">Data Armada</CNavLink>
+              </CNavItem>
+            </CNav>
+            <CCardBody className="mx-3">
+              <CRow>
+                <CCol xs={2}>Nama PT</CCol>
                 <CCol xs="auto">:</CCol>
                 <CCol>PT. ABC</CCol>
               </CRow>
               <CRow>
-                <CCol xs={4}>Kota</CCol>
-                <CCol xs="auto">:</CCol>
-                <CCol>Kabupaten Boyolali</CCol>
-              </CRow>
-              <CRow>
-                <CCol xs={4}>Alamat Lengkap</CCol>
+                <CCol xs={2}>Alamat Lengkap</CCol>
                 <CCol xs="auto">:</CCol>
                 <CCol>
                   JL Boyolali - Klaten KM 5, Karanglor, RT.03/RW.06, Jurug,
@@ -41,24 +100,24 @@ const ViewPT = () => {
                 </CCol>
               </CRow>
               <CRow>
-                <CCol xs={4}>No. Telpon</CCol>
+                <CCol xs={2}>No. Telpon</CCol>
                 <CCol xs="auto">:</CCol>
                 <CCol>0854887555</CCol>
               </CRow>
               <CRow>
-                <CCol xs={4}>Alamat Email</CCol>
+                <CCol xs={2}>Alamat Email</CCol>
                 <CCol xs="auto">:</CCol>
                 <CCol>abc@abc.co.id</CCol>
               </CRow>
               <CRow>
-                <CCol xs={4}>NPWP</CCol>
+                <CCol xs={2}>NPWP</CCol>
                 <CCol xs="auto">:</CCol>
                 <CCol>9452661770001</CCol>
-              </CRow> */}
-            </CCol>
-          </CRow>
-        </CCardBody>
-      </CCard>
+              </CRow>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
     </CContainer>
   );
 };
